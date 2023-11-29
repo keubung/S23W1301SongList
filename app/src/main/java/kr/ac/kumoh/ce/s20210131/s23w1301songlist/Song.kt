e6210d35dbf03974bbd1d0d836c9506dda8f7f77
keubung <skdud3390@navercom>
2023-11-29 15:18:36 +0900

    SongList 완성

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20210131/s23w1301songlist/Song.kt
@@ -0,0 +1,10 @@
+package kr.ac.kumoh.ce.s20210131.s23w1301songlist
+
+data class Song(
+    val id: Int,
+    val title: String,
+    val singer: String,
+    val rating: Int,
+    val lyrics: String) {
+
+}
